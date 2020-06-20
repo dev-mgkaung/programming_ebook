11e5a54a008b4f87350d45f869d4180df8002917
Mg Kaung <developermgkaung@gmail.com>
2020-06-20 18:43:08 +0630

    large project file structure for easy to main with team members

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:programmingebook/screens/splash/splash_screen.dart';
+import 'package:programmingebook/screens/home/home_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen()
+      home: HomeScreen(),
     );
-
   }
 }
---
+++ lib/constraints/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kPink50 = const Color(0xFFFEEAE6);
+const kPink100 = const Color(0xFFFEDBD0);
+const kPink300 = const Color(0xFFFBB8AC);
+const kPink400 = const Color(0xFFEAA4A4);
+const kBrown900 = const Color(0xFF442B2D);
+const kErrorRed = const Color(0xFFC5032B);
+const kWhite = const Color(0xFFFFFBFA);
+const kBackgroundWhite = Colors.white;
