4f0aa6ea9dfd9549d5068ccf860df9a62a35457c
Mg Kaung <developermgkaung@gmail.com>
2020-06-29 11:47:35 +0630

    add url launcher code in about

--- lib/screens/page/about.dart
+++ lib/screens/page/about.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:programmingebook/constraints/styles.dart';
 import 'package:programmingebook/utils/asset_path.dart';
+import 'package:programmingebook/utils/url_launcher.dart';
 
 class AboutPage
 {
@@ -14,17 +16,36 @@ class AboutPage
         applicationIcon: Image(image: AssetImage(AssetsPath.logopath+"logo.png",),width: 40,height: 40,),
         applicationLegalese: 'Hey Guy , Nice to meet you again. This application is the Flutter bilopalte template for production level project.This project was created by  bloc pattern and viewmodel. Readable and easy to maintain',
         children:[
+//          Padding(
+//            padding: const EdgeInsets.only(top:10.0),
+//            child: FlatButton.icon(
+//              label: Text('Phone- 09979247945',style: Styles.defaultTextStyle,),
+//              icon: Icon(Icons.phone,color: Colors.blueAccent,),
+//              onPressed: (){
+//                UrlLauncher.makePhoneCall('+959979247945');
+//              },
+//            ),
+//          ),
           Padding(
             padding: const EdgeInsets.only(top:10.0),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SvgPicture.asset(AssetsPath.svgpath+"facebook.svg",width: 40,height: 40),
+                GestureDetector(child: SvgPicture.asset(AssetsPath.svgpath+"facebook.svg",width: 40,height: 40),
+                onTap: (){
+                  UrlLauncher.launchInWebViewWithJavaScript('https://www.facebook.com/buddhamobile.apk/');
+                },),
                 SizedBox(width: 20,),
-                SvgPicture.asset(AssetsPath.svgpath+"github.svg",width: 40,height: 40),
+                GestureDetector(child:  SvgPicture.asset(AssetsPath.svgpath+"github.svg",width: 40,height: 40),
+                  onTap: (){
+                    UrlLauncher.launchInWebViewWithJavaScript('https://github.com/dev-mgkaung');
+                  },),
                 SizedBox(width: 20,),
-                SvgPicture.asset(AssetsPath.svgpath+"gmail.svg",width: 40,height: 40),
+                GestureDetector(child:   SvgPicture.asset(AssetsPath.svgpath+"gmail.svg",width: 40,height: 40),
+                  onTap: (){
+                    UrlLauncher.launchInWebViewWithJavaScript('https://www.google.com');
+                  },),
               ],
             ),
           ),
--- lib/utils/url_launcher.dart
+++ lib/utils/url_launcher.dart
@@ -1,7 +1,84 @@
 import 'package:url_launcher/url_launcher.dart';
 
 class UrlLauncher {
+
   static Future launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  static Future<void> launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  static Future<void> launchInWebViewOrVC(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: true,
+        forceWebView: true,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  static Future<void> launchInWebViewWithJavaScript(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: true,
+        forceWebView: true,
+        enableJavaScript: true,
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  static Future<void> launchInWebViewWithDomStorage(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: true,
+        forceWebView: true,
+        enableDomStorage: true,
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  static Future<void> launchUniversalLinkIos(String url) async {
+    if (await canLaunch(url)) {
+      final bool nativeAppLaunchSucceeded = await launch(
+        url,
+        forceSafariVC: false,
+        universalLinksOnly: true,
+      );
+      if (!nativeAppLaunchSucceeded) {
+        await launch(
+          url,
+          forceSafariVC: true,
+        );
+      }
+    }
+  }
+
+  static Future<void> makePhoneCall(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
